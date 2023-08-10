a358544e3253e54357d00b62a019f8351fa7ebc7
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-10 09:27:11 +0530

    view engine ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const flash = require('connect-flash');
 const exec = require('child_process').exec;
+const path = require('path'); // Import the path module
 
 const app = express();
 const port = 3000;
@@ -11,9 +12,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(flash());
 
+// Set EJS as the view engine and configure views directory
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // Routes
 app.get('/', (req, res) => {
-    res.render('index.ejs', { message: req.flash('message') });
+    res.render('index', { message: req.flash('message') }); // 'index' refers to index.ejs
 });
 
 app.post('/runCommand', (req, res) => {
